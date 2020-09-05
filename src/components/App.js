9679d5c2db463e11dbbdbb7acce9bc50503f81f7
Sahil Sarpal <sahilsarpal15@gmail.com>
2020-09-05 23:49:27 +0530

    Added user profile section

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import * as React from 'react';
 import { Timeline } from './timeline/timeline';
-import { Button, InputGroup, FormControl, Container,Jumbotron } from 'react-bootstrap';
+import { 
+          Button, 
+          InputGroup, 
+          FormControl, 
+          Container,
+          Jumbotron,
+          Card,
+          Row,
+          Col,
+          Figure
+        } from 'react-bootstrap';
 
 
 export function App({ initialData }) {
   const [count, setCount] = React.useState(0);
   const [data, setData] = React.useState({});
+  const [user_data, setUserData] = React.useState({});
   let myInput = null;
   var getGitubStuff = () => {
     let username = document.getElementById("input-username").value;
+    fetch(`https://api.github.com/users/${username}`)
+      .then(response => response.json())
+      .then(data => {
+        setUserData(data)
+      })
     fetch(`https://api.github.com/users/${username}/repos`)
       .then(response => response.json())
       .then(data => {
@@ -19,7 +35,8 @@ export function App({ initialData }) {
 
   return (
     <div>
-       <Container className="p-3">
+
+       <Container >
         <Jumbotron>
           <h1>{initialData.appName}</h1>
           <InputGroup className="mb-3">
@@ -44,9 +61,41 @@ export function App({ initialData }) {
           </InputGroup>
         </Jumbotron>
       </Container>
-    
+      
       {
-        (count > 0) && <Timeline repos={data} />
+        (count > 0) && 
+
+        <Container >
+  <Row>
+    <Col>
+    <Figure>
+      <Figure.Image
+        width={171}
+        height={180}
+        alt="171x180"
+        src={user_data.avatar_url}
+      />
+      <Figure.Caption>
+      <p>{user_data.name}</p>
+      <p>{user_data.bio}</p>
+      </Figure.Caption>
+    </Figure>
+    </Col>
+    <Col>
+      <Card>
+        <Card.Header>Timeline</Card.Header>
+        <Card.Body>
+          <Timeline repos={data} />
+          <footer className="blockquote-footer">
+              powered by <cite title="Sahil Sarpal">Sahil Sarpal</cite>
+            </footer>
+        </Card.Body>
+      </Card>
+    </Col>
+  </Row>
+</Container>
+       
+
       } 
     </div>
   );
